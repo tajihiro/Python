59141820e326d70cbaaca7fa8a4915bd9a2f234c
H.Tajima <tajihiro2@gmail.com>
2017-05-13 13:50:50 +0900

    Listプログラムの追加

---
+++ Begginer/average.py
@@ -0,0 +1,13 @@
+score_list = [34, 54, 98, 43, 56, 70, 68]
+total = 0
+for score in score_list:
+  total += score
+  print("累積：" + str(total))
+
+print("-----------")
+print("合計：{}点 / 合計数：{}".format(total,len(score_list)))
+ave = total / len(score_list)
+print("平均：{:.1f}点".format(ave))
+
+print("-----------")
+print("合計：" + str(sum(score_list)))
